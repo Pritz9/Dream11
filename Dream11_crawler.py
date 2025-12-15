0336e76e0399e32d8315a340ba34eaee318e863f
Pritz9 <161838190+Pritz9@users.noreply.github.com>
2025-12-15 21:49:13 +0530

    Refactor Dream11 crawler code structure

--- Dream11_crawler.py
+++ Dream11_crawler.py
@@ -2,6 +2,8 @@ import pickle
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import time
+
+##from bs4 import BeautifulSoup as bs by Priti
 ##pickle.dump(chrome.get_cookies(), open("cookie.txt","wb"))
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -49,4 +51,5 @@ data=pd.DataFrame(zip(team_list[1:],player_list[0::11],player_list[1::11],player
                       player_list[5::11],player_list[6::11],player_list[7::11],player_list[8::11],player_list[9::11],player_list[10::11]),
                   columns=["TEAM","Pl-1","Pl-2","Pl-3","Pl-4","Pl-5","Pl-6","Pl-7","Pl-8","Pl-9","Pl-10","Pl-11"])
 data.to_excel("DREAM11.xlsx")
+
 
